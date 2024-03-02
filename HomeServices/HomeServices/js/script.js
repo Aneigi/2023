15ee95a209de05bbc083c02686f5cca7a712c47e
Aneigi <122029056+Aneigi@users.noreply.github.com>
2024-03-02 11:44:03 +0200

    Delete HomeServices/HomeServices directory

--- HomeServices/HomeServices/js/script.js
+++
@@ -1,8 +0,0 @@
-const iconMenu = document.querySelector('.icon-menu');
-if (iconMenu) {
-    const MenuBody = document.querySelector('.menu__body');
-    iconMenu.addEventListener('click', function(a) {
-        iconMenu.classList.toggle('active');
-        MenuBody.classList.toggle('active');
-    });
-}
